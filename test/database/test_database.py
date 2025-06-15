266508fcc5eb1d8961f7a5c2f11916b317861d3c
Jose Colomer <joseluis.colomer@tiko.energy>
2025-06-15 21:16:42 +0200

    feat: create test database container and aply migrations

--- test/database/test_database.py
+++ test/database/test_database.py
@@ -1,9 +1,18 @@
-import pytest
-import docker
-
 from pathlib import Path
 
+import docker
+import psycopg2
+import pytest
+import sqlalchemy
+import subprocess
 import time
+import os
+
+import alembic
+from alembic.config import Config
+from alembic import command, autogenerate
+from alembic.script import ScriptDirectory
+from alembic.runtime.environment import EnvironmentContext
 
 DATABASE_CONTAINER_NAME = "device-sharing"
 DATABASE_CONTAINER_PORT = 5432
@@ -17,6 +26,7 @@ MIGRATIONS_FOLDER = Path(__file__).parents[1] / "migrations"
 def docker_client():
     return docker.from_env()
 
+
 @pytest.fixture
 def database_port(docker_client):
     container_port = DATABASE_CONTAINER_PORT
@@ -24,6 +34,7 @@ def database_port(docker_client):
         if str(container_port) in str(container.attrs["NetworkSettings"]["Ports"]):
             container_port += 10
     return container_port
+
 
 @pytest.fixture
 def empty_database(database_port, docker_client):
@@ -48,9 +59,38 @@ def empty_database(database_port, docker_client):
     yield
     container.stop()
 
-@pytest.fixture
-def database_url():
-    return f"db:postgres://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost:{DATABASE_CONTAINER_PORT}/{DATABASE_NAME}"
 
-def test_apply_migrations(empty_database):
+@pytest.fixture
+def database_url(empty_database):
+    return f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost:{DATABASE_CONTAINER_PORT}/{DATABASE_NAME}"
+
+
+@pytest.fixture
+def database_connection(database_url):
+    sqlalchemy.url = database_url
+    engine = sqlalchemy.create_engine(database_url)
+    return engine.connect()
+
+
+@pytest.fixture
+def set_up_alembic(database_url, database_connection):
+    alembic_cfg = Config()
+    alembic_cfg.set_main_option("script_location", "migrations")
+    alembic_cfg.set_main_option("sqlalchemy.url", database_url)
+
+    alembic_script = ScriptDirectory.from_config(alembic_cfg)
+    alembic_env = EnvironmentContext(alembic_cfg, alembic_script)
+    alembic_env.configure(connection=database_connection)
+    alembic_context = alembic_env.get_context()
+
+    command.upgrade(alembic_cfg, "head")
+
+
+@pytest.fixture
+def database(empty_database, set_up_alembic):
+    set_up_alembic
+
+
+def test_downgrade_migration():
+    # subprocess.Popen("alembic downgrade base", shell=True)
     pass
